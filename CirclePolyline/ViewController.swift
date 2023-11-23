d85ebe13aa481f8f7bde74f7cc84a87408b74c83
Akash <akashpradhan439@gmail.com>
2023-11-23 12:55:30 +0530

    using symbol style layer

--- CirclePolyline/ViewController.swift
+++ CirclePolyline/ViewController.swift
@@ -1,21 +1,14 @@
-//
-//  ViewController.swift
-//  CirclePolyline
-//
-//  Created by MMI on 16/11/23.
-//
-
 import UIKit
 import MapplsAPICore
 import MapplsAPIKit
 import MapplsMap
+import CoreLocation
 
 class ViewController: UIViewController {
     private let directions: Directions = Directions.shared
     private var mapView: MapplsMapView!
     private let centerButton: UIButton = UIButton()
-    private var polyineAnnotations: MGLPolyline!
-    private var coordinates: [CLLocationCoordinate2D] = []
+    private var coordinates : [CLLocationCoordinate2D] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,33 +21,6 @@ class ViewController: UIViewController {
         mapView = MapplsMapView(frame: view.frame)
         view.addSubview(mapView)
         mapView.delegate = self
-        let waypoints = [
-            Waypoint(coordinate: CLLocationCoordinate2D(latitude: 23.337517053873405, longitude: 85.31862815574387), name: "SomeName"),
-            Waypoint(coordinate: CLLocationCoordinate2D(latitude: 23.333273375059115, longitude: 85.32171329787606), name: "name"),
-        ]
-
-        let options = RouteOptions(waypoints: waypoints, resourceIdentifier: .routeAdv, profileIdentifier: .driving)
-        options.includesSteps = true
-
-        _ = directions.calculate(options) { (waypoints, routes, error) in
-            guard error == nil else {
-                print("Error calculating directions: \(error!)")
-                return
-            }
-            
-            if let route = routes?.first, let _ = route.legs.first {
-                MapplsMapAuthenticator.sharedManager().initializeSDKSession {[self] isSucess, error in
-                    if isSucess {
-                        polyineAnnotations = MGLPolyline(coordinates: route.coordinates!, count: UInt(route.coordinates!.count))
-                        
-                        coordinates = route.coordinates!
-                        centerButton.addTarget(self, action: #selector(self.centerMapView), for: .touchUpInside)
-                    } else {
-                        print("error: \(error!.localizedDescription)")
-                    }
-                }
-            }
-        }
     }
     
     func setCenterButton(){
@@ -74,13 +40,77 @@ class ViewController: UIViewController {
     }
     
     @objc func centerMapView(){
-        mapView.showAnnotations([polyineAnnotations], animated: true)
+        mapView.showAnnotations([MGLPolyline(coordinates: &coordinates, count: UInt(coordinates.count))], animated: true)
     }
 
     private func setDottedPolyline(){
-        
+        let waypoints = [
+            Waypoint(coordinate: CLLocationCoordinate2D(latitude: 23.337517053873405, longitude: 85.31862815574387), name: "SomeName"),
+            Waypoint(coordinate: CLLocationCoordinate2D(latitude: 23.333273375059115, longitude: 85.32171329787606), name: "name")
+        ]
+
+        let options = RouteOptions(waypoints: waypoints, resourceIdentifier: .routeAdv, profileIdentifier: .driving)
+        options.includesSteps = true
+
+        _ = directions.calculate(options) { (waypoints, routes, error) in
+            guard error == nil else {
+                print("Error calculating directions: \(error!)")
+                return
+            }
+            
+            if let route = routes?.first, let _ = route.legs.first {
+                MapplsMapAuthenticator.sharedManager().initializeSDKSession {[self] isSucess, error in
+                    if isSucess {
+                        coordinates = route.coordinates!
+                        mapView.showAnnotations([MGLPolyline(coordinates: &coordinates, count: UInt(coordinates.count))], animated: true)
+                        
+                        let source = MGLShapeSource(identifier: "line", shape: MGLPolyline(coordinates: &coordinates, count: UInt(coordinates.count)) , options: nil)
+                        mapView.style?.addSource(source)
+                        let layer = dotRouteStyleLayer(identifier: "line-dotted", source: source)
+                        mapView.style?.addLayer(layer)
+                        
+                        centerButton.addTarget(self, action: #selector(self.centerMapView), for: .touchUpInside)
+                    } else {
+                        print("error: \(error!.localizedDescription)")
+                    }
+                }
+            }
+        }
     }
     
+    public let MBRouteDotSpacingByZoomLevel: [Double: NSExpression] = [
+        1: NSExpression(forConstantValue:1.0),
+        2: NSExpression(forConstantValue:1.0),
+        3: NSExpression(forConstantValue:1.0),
+        4: NSExpression(forConstantValue:1.0),
+        5: NSExpression(forConstantValue:1.0),
+        6: NSExpression(forConstantValue:2.0),
+        7: NSExpression(forConstantValue:3.0),
+        8: NSExpression(forConstantValue:4.0),
+        9: NSExpression(forConstantValue:5.0),
+        10: NSExpression(forConstantValue:6.0),
+        22: NSExpression(forConstantValue:30.0)
+    ]
+
+    func dotRouteStyleLayer(identifier: String, source: MGLSource) -> MGLStyleLayer {
+        let shapeLayer = MGLSymbolStyleLayer(identifier: identifier, source: source)
+        
+        if let style = mapView.style, style.image(forName: "dot-image") == nil,  let image = UIImage(named: "dotImage") {
+            style.setImage(image, forName: "dot-image")
+        }
+        
+        shapeLayer.minimumZoomLevel = 3.0
+        shapeLayer.iconImageName = NSExpression(forConstantValue: "dot-image")
+        
+        shapeLayer.iconAllowsOverlap = NSExpression(forConstantValue: true)
+        shapeLayer.symbolPlacement =  NSExpression(forConstantValue: NSValue(mglSymbolPlacement: .line))
+        //format: "mgl_interpolate:withCurveType:parameters:stops:($zoomLevel, 'linear', nil, %@)", MBRouteDotSpacingByZoomLevel
+        shapeLayer.symbolSpacing =  NSExpression(forConstantValue: 2)
+        shapeLayer.iconScale = NSExpression(forConstantValue: 0.4)
+        
+        return shapeLayer
+    }
+
     func hexStringToUIColor (hex:String) -> UIColor {
         var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
 
@@ -111,38 +141,11 @@ extension ViewController : MapplsMapViewDelegate {
     
     func mapViewDidFinishLoadingMap(_ mapView: MGLMapView) {
         print("mapViewDidFinishLoadingMap")
-        if let polyineAnnotations = polyineAnnotations {
-            var someFeature: [MGLPointFeature] = []
-            mapView.showAnnotations([polyineAnnotations], animated: true)
-            for coordinate in coordinates {
-                let point = MGLPointFeature()
-                point.coordinate = coordinate
-                someFeature.append(point)
-            }
-            let source = MGLShapeSource(identifier: "line", features: someFeature, options: [
-                .clustered : true,
-                .clusterRadius: 15
-            ])
-            mapView.style?.addSource(source)
-            
-            let circleLayer = MGLCircleStyleLayer(identifier: "circle", source: source)
-            circleLayer.circleStrokeColor = NSExpression(forConstantValue: UIColor.black)
-            circleLayer.circleStrokeOpacity = NSExpression(forConstantValue: 1)
-            circleLayer.circleStrokeWidth = NSExpression(forConstantValue: 1)
-            circleLayer.circleColor = NSExpression(forConstantValue: hexStringToUIColor(hex: "#03adfc"))
-            circleLayer.circleRadius = NSExpression(forConstantValue: 4)
-            circleLayer.circleOpacity = NSExpression(forConstantValue: 1)
-            
-            mapView.style?.addLayer(circleLayer)
-        } else {
-            let action = UIAlertController(title: "Connection error", message: "", preferredStyle: .alert)
-            action.isSpringLoaded = true
-            action.addAction(UIAlertAction(title: "Dismiss", style: .default))
-            self.present(action, animated: true)
-        }
+        setDottedPolyline()
     }
     
     func didSetMapplsMapStyle(_ mapView: MapplsMapView, isSuccess: Bool, withError error: Error?) {
         print("didSetMapplsMapStyle")
     }
 }
+
